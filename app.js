6948cead807034e7e6fd7c12daace20375f5f8d4
Mayank Verma <vermamayank714@gmail.com>
2023-03-08 15:42:08 +0530

    How to send the respinse back to the server

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 // Require the http module
-const http = require('http');
+const http = require("http");
+// const app = express();
 const server = http.createServer((req, res) => {
-    console.log(req);
+  // console.log(req) // To see the request body
+  //    To write the response
+  res.setHeader('Content-Type','text/html');
+  res.write('<html>')
+  res.write('<head><title>My first page</title></head>')
+  res.write('<body><h1>My server is running</h1></body>')
+  res.write('</html>')
+  res.end()
+
+  // process.exit(); // To stop the server
 });
 server.listen(3000);
-
